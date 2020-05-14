51f56dec56dc6f6980e8fe0fb54d975a53790c20
Drewbi <drewalexander986@gmail.com>
2020-05-14 17:35:17 +0800

    Changed scoring range

--- src/data/todos.js
+++ src/data/todos.js
@@ -1,24 +1,24 @@
 export default [{
   text: 'Increase sustainable energy production',
-  urgency: 7.5,
-  difficulty: 3.5,
+  urgency: 75,
+  difficulty: 35,
   complete: false
 },
 {
   text: 'Decrease greenhouse gas emissions',
-  urgency: 5.5,
-  difficulty: 6.4,
+  urgency: 55,
+  difficulty: 64,
   complete: false
 },
 {
   text: 'Invest in renewable resources',
-  urgency: 4.6,
-  difficulty: 3.7,
+  urgency: 46,
+  difficulty: 37,
   complete: false
 },
 {
   text: 'Walk dog',
-  urgency: 0.3,
-  difficulty: 2.3,
+  urgency: 3,
+  difficulty: 23,
   complete: true
 }]
