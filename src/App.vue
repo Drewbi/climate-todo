0561ac174089ea8db71fc8712843ff579059b955
Drewbi <drewalexander986@gmail.com>
2020-05-14 17:34:28 +0800

    Added sorting and add button

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <Header title="Climate Todo" subtitle="Take the Next Step" />
-    <TodoList title="Remaining Steps" :items="getIncomplete">All steps complete!</TodoList>
-    <TodoList title="Steps Completed" :items="getComplete">No completed steps</TodoList>
+    <TodoList title="Remaining Steps" :items="incompleteSorted" emptyText="All steps complete!">
+      <div id="addButton" @click="addModal = true">Add Step</div>
+    </TodoList>
+    <AddTodoModal v-if="addModal" @close="addModal = false"/>
+    <TodoList title="Steps Completed" :items="getComplete" emptyText="No completed steps" />
   </div>
 </template>
 
@@ -10,16 +13,29 @@
 import seeds from "./data/todos"
 import Header from './components/Header.vue'
 import TodoList from './components/TodoList.vue'
+import AddTodoModal from './components/AddTodo.vue'
 import { mapGetters, mapMutations } from 'vuex'
 
 export default {
   name: 'App',
   components: {
     Header,
-    TodoList
+    TodoList,
+    AddTodoModal
   },
+  data: () => ({
+    addModal: false
+  }),
   computed: {
-    ...mapGetters(['getComplete', 'getIncomplete'])
+    ...mapGetters(['getComplete', 'getIncomplete']),
+    incompleteSorted(){
+      const sortingArray = [...this.getIncomplete]
+      return sortingArray.sort((todo1, todo2) => {
+        const score1 = todo1.urgency / todo1.difficulty
+        const score2 = todo2.urgency / todo2.difficulty
+        return score2 - score1
+      })
+    }
   },
   methods: {
     ...mapMutations(['addTodo'])
@@ -40,5 +56,15 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+#addButton {
+  width: 200px;
+  margin-top: 10px;
+  padding: 10px;
+  background-color: #daf7ee;
+  border-radius: 5px;
+}
+#addButton:hover {
+  background-color: #d4f2e9;
 }
 </style>
